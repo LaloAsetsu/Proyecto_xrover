eb418c5915409a58cb77e88a52ba4a25472b1bf1
Angel Bosquez <A01667100@tec.mx>
2024-11-28 17:38:07 -0600

    Comentarios en ControlButtons.tsx

--- app/components/ControlButtons.tsx
+++ app/components/ControlButtons.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import React from 'react';
+//estos son lso botones con los que se podria enviar los comandos para el movimiento de carrito
+//envirian w,a,s y d 
 
 const ControlButtons: React.FC = () => {
   const sendCommand = async (command: string): Promise<void> => {
@@ -12,17 +14,19 @@ const ControlButtons: React.FC = () => {
       });
 
       const result = await response.json();
-      console.log(result.message);
-    } catch (error) {
+      console.log(result.message);   //mensaje de respuesta
+    } catch (error) {             //manejo de erroes
       console.error('Error al enviar el comando:', error);
     }
   };
 
   return (
+  //el ontenedor principal del panel de control, con fondo blanco, bordes redondeados y efecto de sombreado
     <div className="control-panel sticky top-4 bg-white p-4 rounded shadow-md">
       <div className="flex flex-col items-center">
-        {/* Fila de botones WASD */}
+        {/*botones  WASD */}
         <div className="flex gap-2">
+   {/*el boton de W y S tienen un fondo verde, texto blanco y bordes redondeados */}
           <button
             onClick={() => sendCommand('w')}
             className="bg-green-500 text-white p-8 rounded-lg w-24"
@@ -32,6 +36,7 @@ const ControlButtons: React.FC = () => {
         </div>
 
         <div className="flex gap-2 my-2">
+  {/* los botones A y D teinen fondo amarillo, texto blanco y bordes redondeados */}
           <button
             onClick={() => sendCommand('a')}
             className="bg-yellow-500 text-white p-8 rounded-lg w-24"
@@ -52,8 +57,9 @@ const ControlButtons: React.FC = () => {
           </button>
         </div>
 
-        {/* Botón de Parar debajo */}
+        {/*boton de PARAR debajo de los botones de control */}
         <div className="mt-4">
+   {/* por convencion el color rojo se asocia con errores o urgencia, asi que nuestro boton PARAR tiene este color */}
           <button
             onClick={() => sendCommand('stop')}
             className="bg-red-500 text-white p-8 rounded-lg w-24"
