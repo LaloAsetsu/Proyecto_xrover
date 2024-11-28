c37df47510785d2f0087305521d7c4d4b68369d1
Angel Bosquez <A01667100@tec.mx>
2024-11-28 17:46:45 -0600

    Comentarios en  Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 // src/app/components/Footer.tsx
+//Footer se refiere al pie de pagina de nuestra app
 export default function Footer() {
+    //cuenta con un fondo gris claro, el texto aparece centrado y tiene un margen superior
     return (
       <footer className="bg-gray-200 text-center p-4 mt-6">
         <p>Equipo 8 IoT</p>
