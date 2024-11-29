012bd1b4c1d4df71b3fbc04deee565465e5f13e3
Angel Bosquez <A01667100@tec.mx>
2024-11-28 18:31:47 -0600

    Comentarios en Header.tsx

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 // src/app/components/Header.tsx
+
+//el Header es el encabezado de nuestro sitio, solo añade el texto que se observa abajo 
 export default function Header() {
     return (
       <header className="header">
